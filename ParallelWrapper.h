a97d544b2e3dd34c06b85dc2e0bd3a09b0d7609b
Aidan Haile <tacticalfluke304@gmail.com>
2021-06-30 11:32:12 +0800

    Add comments and correct header comment

--- ParallelWrapper.h
+++ ParallelWrapper.h
@@ -1,6 +1,20 @@
-//
-// Created by Aidan Haile on 29/6/21.
-//
+/*
+ * 	DupeDetect: small terminal application to find duplicate files
+ *  Copyright (C) 2021  Aidan Haile
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
 
 #ifndef DUPEDETECT_PARALLELWRAPPER_H
 #define DUPEDETECT_PARALLELWRAPPER_H
@@ -14,6 +28,19 @@
 
 namespace DupeDetect
 {
+	/**
+	 * System independence wrapper around a parallel for loop
+	 *
+	 * @note On systems other than MacOS, this simply dispatches the arguments to the standard library parallel
+	 * for implementation. On MacOS, it uses Grand Central Dispatch to queue up the closure with each item in the
+	 * span.
+	 *
+	 * @tparam iterator_t iterator type
+	 * @tparam closure_t closure type, gets run on every object defined betwen the start and end iterators
+	 * @param start the start of span of items to iterate over
+	 * @param end past the end iterator for the items to iterate over
+	 * @param closure closure to run over every item iterated over. Must be thread-safe in its context
+	 */
     template <class iterator_t, class closure_t>
     void parallelFor(iterator_t&& start, iterator_t&& end, closure_t&& closure)
     {
